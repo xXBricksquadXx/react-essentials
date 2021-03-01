bc312bb950874d283a4ce95e9735e77bc7eac678
colby <colby88cardell@gmail.com>
2021-03-01 01:29:45 -0500

    :sparkles: displaying data from an api

--- src/App.js
+++ src/App.js
@@ -14,7 +14,11 @@ function App({ login }){
   }, []);
 
   if (data) {
-    return<div>{JSON.stringify(data)}</div>;
+    return(<div>
+      <h1>{data.name}</h1>
+      <p>{data.location}</p>
+      <img alt={data.login} src={data.avatar_url}/>
+    </div>);
   }
 
   return <div>No User Available</div>;
