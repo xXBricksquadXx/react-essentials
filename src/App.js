61bd18fc8fb74291d069480fdac797890b8ddc22
colby <colby88cardell@gmail.com>
2021-03-01 00:58:44 -0500

    :sparkles: incorporating useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
-import { useState} from 'react';
+import { useReducer, useState} from 'react';
 import './App.css';
 
 function App(){
-  const [checked, setChecked] = useState(false );
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false );
+
+
+
+
   return (
     <>
-<input type="checkbox" value={checked} onChange={() => setChecked(checked => !checked)} />
+<input 
+type="checkbox" 
+value={checked} 
+onChange={toggle} />
 <p>{checked ? "checked" : "not checked"}</p>
     </>
   )
