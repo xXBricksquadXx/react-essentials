2903a8c05dadd752b210b3c74341a6b5c1d80001
colby <colby88cardell@gmail.com>
2021-02-28 22:10:42 -0500

    Adding component properties

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 
-function Header () {       // A component is a function that returns UI
-  return (
+function Header (props) { 
+      console.log(props);  // pass props into function and hold all of the properties for this component
+  return (                                      // props name has been passed as a key
     <header>
-      <h1>The Business</h1>
+      <h1>{props.name} Business</h1> 
     </header>
 
   );
@@ -33,7 +34,7 @@ function Footer(){
 function App() {
   return (                                                      //create a component by creating a function, the function returns jsx
     <div className="App">
-    <Header />                                          
+    <Header  name="MY"/>                                    
     <Main />
     <Footer />
      
