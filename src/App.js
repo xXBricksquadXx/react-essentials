c8b9afb4a16df39f70b61f34aa85aefa3b57f302
colby <colby88cardell@gmail.com>
2021-02-28 21:52:16 -0500

    :construction: Render the header inside of the App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+function Header () {       // A component is a function that returns UI
+  return (
+    <header>
+      <h1>The Business</h1>
+    </header>
+
+  );
+}
+
+
+
+
 function App() {
-  return (                                                                                                        //create a component by creating a function, the function returns jsx
+  return (                                                      //create a component by creating a function, the function returns jsx
     <div className="App">
+    <Header />                                          
+    <h2>Main</h2>
+    <h3>Footer</h3>
      
     </div>
   );
