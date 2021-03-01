6bf27d16eb93d241242776b27606b82dc1a1cb77
colby <colby88cardell@gmail.com>
2021-03-01 01:46:44 -0500

    :sparkles: Handle loading states

--- src/App.js
+++ src/App.js
@@ -6,12 +6,23 @@ import './App.css';
 
 function App({ login }){
   const [data, setData] = useState(null);
+  const [loading, setLoading] =useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    if (!login) return;
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
     .then(response =>response.json())
-    .then(setData);
-  }, []);
+    .then(setData)
+    .then(()=> setLoading(false))
+    .catch(setError);
+  }, [login]);    // any time the login value changes thats when we call it
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error)
+  return <pre>{JSON.stringify(error,null,2)}</pre>;
+  if(!data) return null;
 
   if (data) {
     return(<div>
@@ -19,10 +30,7 @@ function App({ login }){
       <p>{data.location}</p>
       <img alt={data.login} src={data.avatar_url}/>
     </div>);
-  }
-
-  return <div>No User Available</div>;
-
+  };
 }
 
 export default App;
