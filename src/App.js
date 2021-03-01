d0776fbf106363a43c829946762c30679b73289a
colby <colby88cardell@gmail.com>
2021-02-28 21:42:48 -0500

    :construction: Import App

--- src/App.js
+++ src/App.js
@@ -2,24 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  return (                                                                                                        //create a component by creating a function, the function returns jsx
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
 
-export default App;
+export default App;    // need 2 import in index
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from "./App"
 
 
 
-ReactDOM.render(
-<ul>
-  <li>Monday</li>
-  <li>Tuesday</li>
-  <li>Wednesday</li>
-</ul>,
+ReactDOM.render(<App />,
   document.getElementById('root') // second argument is where we want to put it.
 );
 
