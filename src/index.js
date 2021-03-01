0d1ecd17e0a425d58fe617067449069d1230678c
colby <colby88cardell@gmail.com>
2021-02-28 21:23:37 -0500

    :sparkles: React.createElement property change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 
 ReactDOM.render(
-React.createElement("h1", null, "Heyyyy Everyones!!!"),
-  document.getElementById('root')
+React.createElement("h1", {style: {color: "blue"}}, "Bricksquad!!!"), //first argument  is what we want to create
+  document.getElementById('root') // second argument is where we want to put it.
 );
 
